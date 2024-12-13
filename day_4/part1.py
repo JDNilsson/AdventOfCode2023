c8f80edeccbf4a30d183995c57261ec77481398b
JDNilsson <daniel.nilsson0107@gmail.com>
2024-12-13 12:35:41 +0100

    Completed Day 4, Part 1

--- day_4/part1.py
+++ day_4/part1.py
@@ -1,8 +1,48 @@
+import re
+
+#with open("/home/danne/Git_Repos/AdventOfCode2024/day_4/test","r") as f:
 with open("/home/danne/Git_Repos/AdventOfCode2024/day_4/input","r") as f:
-   lines = f.read().split('\n')
+    lines = f.read().split('\n')
+
+m = len(lines)
+n = len(lines[0])
+off = 0
+for line in lines:
+    if (not (len(line) == n)): 
+        off += 1
+print(off)
+def find_XMAS(i,j,x,y):
+    return ((lines[i+y][j+x] == 'M') and 
+        (lines[i+2*y][j+2*x] == 'A') and 
+        (lines[i+3*y][j+3*x] == 'S'))
 
 def main():
-    return
+    forwad_reg = 'XMAS'
+    backward_reg = 'SAMX'
+    count = 0
+    
+    for line in lines:
+        count += len(re.findall(forwad_reg,line))
+        count += len(re.findall(backward_reg,line))
+    for i in range(m):
+        for j in range(n):
+            if (lines[i][j] == 'X'):
+                if (i >= 3):
+                    count += find_XMAS(i,j,0,-1)
+                    if (j >= 3):
+                        count += find_XMAS(i,j,-1,-1)
+                    if (j <= len(lines[i])-4):
+                        count += find_XMAS(i,j,1,-1)
+                if (i <= len(lines)-4):
+                    count += find_XMAS(i,j,0,1)
+                    if (j >= 3):
+                        count += find_XMAS(i,j,-1,1)
+                    if (j <= len(lines[i])-4):
+                        count += find_XMAS(i,j,1,1)
+    print(count)
+
+
+
 
 if __name__ == "__main__":
     main()
